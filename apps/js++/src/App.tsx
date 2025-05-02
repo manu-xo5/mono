dcdbb6dc330254cc4d96bfe8b77cc6615d040a16
manu-xo5 <mohit.matwaya@gmail.com>
2025-05-02 12:09:14 +0530

    git sync

--- apps/js++/src/App.tsx
+++ apps/js++/src/App.tsx
@@ -1,16 +1,32 @@
+import { OP, Runtime } from "@/runtime/index";
 import { useState } from "react";
-import { Runtime } from "./runtime";
 
+function CustomDslComp() {
+    return new Runtime("button",[
+           [OP.PSH, "hello world"],
+           [OP.PSH, 1],
+           [OP.CAL],
+           [OP.PSH,"Hello"],
+           [OP.STP, "children"],
+           [OP.ELM, "button" ],
+           [OP.HALT],
+           [OP.NATC, "log"],
+           [OP.RET],
+           // logHello
+         ]).execute();
+}
+
+console.log(CustomDslComp())
 
 function App() {
     const [count, setCount] = useState(0);
-      const x = Runtime.runMachine();
 
     return (
         <div>
+            <CustomDslComp />
             <button onClick={() => setCount(count => count + 1)}>
                 count is
-                {x}{" "}
+                {" "}
                 {count}
             </button>
         </div>
--- apps/js++/src/runtime/index.ts
+++ apps/js++/src/runtime/index.ts
@@ -1,146 +1,186 @@
-import type * as React from "react";
-
-type data_type = any;
-
-// const insArr_literal = [
-//    ["MOV_LIT_REG", "count", "value123"],
-//    ["RET_REG", "count", "", ""],
-//    // ["RET_LIT", "count", "", ""],
-// ] as [string, string, string, data_type ][];
-
-// example for array literal with copy,
-// same applied for objects
-// const insArr_arr = [
-//    ["MOV_LIT_REG", "count", [12, 3, 2]],
-//    ["MEM_CPY", "count3", "count"],
-//    ["MOV_REG_REG", "count2", "count"],
-//    ["RET_REG", "count2", "", ""],
-//    // ["RET_LIT", "count", "", ""],
-// ] as [string, string, string, data_type ][];
+import { createElement } from "react";
+import * as React from "react";
 
 /**
- * @example for jump
- * const insArr_jump = [
- *     [OP.MOV_LIT_REG, "count", [12, 3, 2]],
- *     [OP.CPY_REG_REG, "count3", "count"],
- *     [OP.MOV_REG_REG, "count2", "count"],
- *     [OP.JMP, "5"],
- *     [OP.RET_REG, "count2", "", ""],
- *     [OP.MOV_LIT_REG, "x", 1],
- *     [OP.RET_REG, "x"],
- * ] as [number, string, string, data_type][];
+ * component:
+ *   RENDER_START
+ *   PSH "hello"
+ *   APC
+ *   RENDER_END
  */
 
-enum OP {
-    MOV_LIT_REG,
-    MOV_REG_REG,
-    CPY_REG_REG,
-    RET_REG,
-    JMP,
+/**
+ * <button onClick={handleClick}>Hello</button>
+ * <button>World</button>
+ *
+ * button:
+ *    onClick: handleClick
+ *    children: [ Hello ]
+ * button:
+ *    children: [ World ]
+ *
+ * PSH Hello
+ * STP children
+ * LOD handleClick
+ * STP onClick
+ * ELM button
+ * 
+ * PUSH World
+ * SETP children
+ * ELEM button
+ *
+ * RETU 
+ */
+
+export enum OP {
+    // stack operations
+    PSH,
+    POP,
+
+    // regs operations
+    LOD,
+    MOV, // MOVe
+
+    RET, // RETurn
+    HALT, // HALT
+
+    CAL, // CALl
+    NATC, // NATive Code
+
+    FUNC,
+
+    STP,
+    ELM,
 }
 
-type OPERATION = [OP, string, data_type, string][];
+const nativeCode = {
+  "log": (...arg: any[]) => console.log(...arg)
+}
 
-const insArr_jump = {
-    start: [
-        [OP.MOV_LIT_REG, "count", [12, 3, 2], ""],
-        [OP.CPY_REG_REG, "count3", "count", ""],
-        [OP.MOV_REG_REG, "count2", "count", ""],
-        [OP.JMP, "other", "", ""],
-        [OP.RET_REG, "count2", "", ""],
-    ],
-    other: [
-        [OP.MOV_LIT_REG, "x", 1, ""],
-        [OP.RET_REG, "x", "", ""],
-    ],
-} satisfies Record<string, OPERATION>;
+export class Runtime {
+    stack: object[] = [];
+    registers: Record<string, object> = {};
+    propsAcc: Record<string, any> = {};
+    children: React.ReactElement[] = [];
+    labels: Record<string, [number, ...any[]][]> = {};
+    sp: number = 0;
 
-function runMachine<Program extends Record<string, OPERATION>>(program: Program): React.ReactNode {
-    let label: keyof typeof program = "start";
+    constructor(public name: string, public instruction: [number, ...any[]][]) {}
 
-    const mem_stack: unknown[] = [];
-    const heap: Record<
-        string,
-        | { type: "literal"; value: string | number }
-        | { type: "pointer"; value: number }
-    > = {};
+    // Execute opcode
+    execute() {
+        for (let ip = 0; ip < this.instruction.length; ip++) {
+            const [opcode, ...args] = this.instruction[ip]!;
 
-    try {
-        let ip = 0;
-        while (ip < program[label]!.length) {
-            const ins = program[label]![ip]!;
-            const [opCode, ...args] = ins;
-
-            switch (opCode) {
-                case OP.JMP: {
-                    const [labelTarget] = args;
-                    ip = 0;
-                    label = labelTarget as keyof typeof program;
-                    continue;
+            console.log(opcode);
+            switch (opcode) {
+                case OP.PSH:
+                    this.stack.push(args[0]);
+                    break;
+                case OP.POP:
+                    this.stack.pop();
+                    break;
+                case OP.LOD: {
+                    const [name] = args;
+                    const value = this.stack.pop()!;
+                    this.registers[name] = value;
+                    break;
                 }
-                case OP.RET_REG: {
-                    const [regName] = args;
-                    const pointer = heap[regName];
-                    if (pointer?.type === "pointer") {
-                        return mem_stack[pointer.value] as string;
+                case OP.STP: {
+                    const name = args[0];
+                    const value = this.stack.pop()!;
+                    this.propsAcc[name] = value;
+                    break;
+                }
+                case OP.ELM: {
+                    const name = args[0];
+                    const props = {...this.propsAcc};
+                    this.propsAcc = {};
+
+                    this.children.push(createElement(
+                      name,
+                      { ...props }
+                    ))
+                    break;
+                }
+
+                case OP.FUNC: {
+                  break
+                }
+                case OP.CAL: {
+                    this.sp = ip;
+                    ip = 7;
+                    console.log(ip, this.sp);
+                    break;
+                }
+                case OP.NATC: {
+                    const name = args[0];
+                    const argN = Number(this.stack.pop()!);
+                    const acc:object[] = [];
+                    let i = 0;
+                    while(i++ < argN) {
+                      acc.push(this.stack.pop()!);
                     }
-                    else {
-                        return pointer?.value ?? 0;
+                    if (!(name in nativeCode)) {
+                        throw new Error(`${name} function not found`);
                     }
-                }
-                case OP.MOV_LIT_REG: {
-                    const [regName, value] = args;
-                    const top = mem_stack.length;
 
-                    heap[regName] = {
-                        type: "pointer",
-                        value: top,
-                    };
-                    mem_stack.push(value);
+                    const fn = nativeCode[name as keyof typeof nativeCode]
+                    if (!fn) {
+                        throw new Error(`${name} function not found`);
+                    }
+
+                    const value = fn()!;
+                    this.stack.push(value);
                     break;
                 }
-                case OP.MOV_REG_REG: {
-                    const [regTo, regFrom] = args;
-                    const pointer = heap[regFrom]!.value as number;
 
-                    heap[regTo] = {
-                        type: "pointer",
-                        value: pointer,
-                    };
-
+                case OP.MOV: {
+                    const [name, value] = args;
+                    this.registers[name] = value;
                     break;
                 }
-                case OP.CPY_REG_REG: {
-                    const [regTo, regFrom] = args;
-                    const pointer = heap[regFrom]!.value as number;
-                    const copiedValue = Object.create(mem_stack[pointer]!);
 
-                    const top = mem_stack.length;
-                    mem_stack.push(copiedValue);
-                    heap[regTo] = {
-                        type: "pointer",
-                        value: top,
-                    };
-
+                case OP.RET: {
+                    ip = this.sp - 1;
                     break;
                 }
+                
+                case OP.HALT: {
+                    const elm = createElement(this.name, { children: this.children });
+                    console.log('returning', elm);
+                    return elm;
+                }
+                default:
+                    throw new Error(`Unknown opcode: ${opcode}`);
             }
-            ip++;
         }
     }
-    finally {
-        // console.dir("---------");
-        // console.dir(JSON.stringify(mem_stack, null, 4));
-        // console.dir(JSON.stringify(heap, null, 4));
+
+    // Start a render block for an element
+    // renderStart(tagOrComponent) {
+    //     this.elementStack.push({
+    //         tag: tagOrComponent,
+    //         props: {},
+    //         children: [],
+    //     });
+    // }
+
+    // End the render block and add to the final element
+    // renderEnd() {
+    //     const ctx = this.elementStack.pop();
+    //     const el = this.createElementOrComponent(ctx);
+    //     if (this.elementStack.length > 0) {
+    //         this.elementStack[this.elementStack.length - 1].children.push(el);
+    //     }
+    //     else {
+    //         this.rootElement = el;
+    //     }
+    // }
+
+    // Call a function (subroutine) and handle arguments from the stack
+    // or built-in JavaScript functions (like `console.log`)
+    callFunc(funcName: string) {
+        
     }
-}
-
-export const Runtime = {
-    runMachine,
-};
-
-declare const Deno: boolean | undefined;
-
-if (Deno) {
-    console.debug(runMachine(insArr_jump));
 }
