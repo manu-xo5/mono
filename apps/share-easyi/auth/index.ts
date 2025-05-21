4c817add8059141ba1c8ea2cf78d3c5594483a3b
manu-xo5 <mohit.matwaya@gmail.com>
2025-05-21 21:12:54 +0000

    chore: something

--- apps/share-easyi/auth/index.ts
+++ apps/share-easyi/auth/index.ts
@@ -5,27 +5,15 @@ import * as schema from "../db/schema/auth.ts";
 
 export const auth = betterAuth({
   emailAndPassword: { enabled: false },
-  trustedOrigins: ["http://localhost:3000", "https://localhost:3000", "https://mono-s.vercel.app"],
-  advanced: {
-    defaultCookieAttributes: {
-      secure: true,
-      httpOnly: true,
-      sameSite: "none",
-    },
-  },
+  trustedOrigins: [
+    "http://localhost:3000",
+    "https://localhost:3000",
+    "https://mono-s.vercel.app",
+  ],
   socialProviders: {
     google: {
       clientId: Deno.env.get("GOOGLE_CLIENT_ID") ?? "",
       clientSecret: Deno.env.get("GOOGLE_CLIENT_SECRET") ?? "",
-      // Change this
-      redirectURI: (() => {
-        const callbackUri = "/api/auth/callback/google";
-        const PUBLIC_DOMAIN = Deno.env.get("RAILWAY_PUBLIC_DOMAIN");
-        if (PUBLIC_DOMAIN) {
-          return "https://" + PUBLIC_DOMAIN + callbackUri;
-        }
-        return "http://localhost:1553" + callbackUri;
-      })(),
     },
   },
 
