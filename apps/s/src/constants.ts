1c06a8675cf5e0b8c97749c56a894d82b4d18072
manu-xo5 <mohit.matwaya@gmail.com>
2025-05-22 14:01:41 +0000

    fhaklsjdkf

--- apps/s/src/constants.ts
+++ apps/s/src/constants.ts
@@ -1,3 +1,3 @@
-export const SERVER_BASE = "https://mono-production.up.railway.app"
+export const SERVER_BASE = import.meta.env.VITE_SERVER_BASE
 export const CALL_RESPONSE_TIMEOUT = 5000 /* 5 seconds */
 
